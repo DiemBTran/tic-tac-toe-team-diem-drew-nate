d116efbd3d76c311ff7a9ebd4836fa23d7ab2914
DiemBTran <diembtran@gmail.com>
2020-09-17 10:37:35 -0700

    we're making the history of Xs and Os to see if they win at 3 in a row

--- src/App.js
+++ src/App.js
@@ -6,15 +6,48 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squareArray: [...Array(9).fill(" ")],
+      xIsNext: true,
+      historyO: [],
+      historyX: []
     }
   }
 
+  marker = (index) =>{
+    let { squareArray, xIsNext, historyO, historyX } = this.state
+    if (squareArray[index] === " "){
+      if (xIsNext === true) {
+        squareArray[index] = "X"
+        xIsNext = false
+        historyX.push(index)
+        console.log("This is X history", historyX);
+      } else {
+        squareArray[index] = "O"
+        xIsNext = true
+        historyO.push(index)
+        console.log("This is O history", historyO);
+      }
+    }
+    this.setState({ squareArray: squareArray})
+    this.setState({ xIsNext: xIsNext})
+  }
+
   render(){
+    let { squareArray } = this.state
+    let squares = squareArray.map((box, index) =>{
+      return(
+        <Square 
+          box = { box }
+          index = { index }
+          key = { index }
+          marker = { this.marker }
+        />
+      )
+    })
     return(
       <React.Fragment>
         <h1>Tic Tac Toe</h1>
-        <Square />
+    <div id="gameboard"> { squares }</div>
       </React.Fragment>
     )
   }
